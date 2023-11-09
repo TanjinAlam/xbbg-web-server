b3206146528aca82667c9e433db587990b528749
TanjinAlam <piash.tanjin@gmail.com>
2023-11-09 17:11:41 +0600

    docker file & other api added

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 
-from schema.request_data import RequestDataBDP, RequestDataBDH
-from utility.helper import async_xbbg
+from schema.request_data import RequestDataBDP, RequestDataBDH, RequestDataBDIB, RequestDataBDS, RequestDataDividend, RequestDataEarning
+from utility.helper import async_xbbg, bdh_async, bdib_async, bdp_async, bds_async, earning_async, get_dividend_data
 
 app = FastAPI()
 
@@ -9,7 +9,7 @@ app = FastAPI()
 async def read_bdp_data(request_body: RequestDataBDP):
     try:
         body_data = request_body
-        response = await async_xbbg(body_data.tickers_name,body_data.fields_name,'bdp', None, None)
+        response = await bdp_async(body_data.tickers_name,body_data.fields_name)
         return response
     except Exception as e:
         print(e)
@@ -18,10 +18,49 @@ async def read_bdp_data(request_body: RequestDataBDP):
 async def read_bdh_data(request_body: RequestDataBDH):
     try:
         body_data = request_body
-        print(body_data)
-        response = await async_xbbg(body_data.tickers_name,body_data.fields_name,'bdh',body_data.start_date, body_data.end_date)
+        response = await bdh_async(body_data.tickers_name,body_data.fields_name,body_data.start_date, body_data.end_date, request_body.adjust)
         return response
     except Exception as e:
         print(e)
+        
+        
+@app.post("/read-bds-data")
+async def read_bds_data(request_body: RequestDataBDS):
+    try:
+        body_data = request_body
+        response = await bds_async(body_data.tickers_name,body_data.fields_name,body_data.start_date, body_data.end_date)
+        return response
+    except Exception as e:
+        print(e)
+        
+@app.post("/read-bdib-data")
+async def read_bdib_data(request_body: RequestDataBDIB):
+    try:
+        body_data = request_body
+        response = await bdib_async(body_data.ticker_name,body_data.session,body_data.query_type,body_data.date)
+        return response
+    except Exception as e:
+        print(e)
+        
+@app.post("/read-earning-data")
+async def read_earning_data(request_body: RequestDataEarning):
+    try:
+        body_data = request_body
+        response = await earning_async(body_data.ticker_name,body_data.by, body_data.query_type, body_data.currency, body_data.level, body_data.date, body_data.period)
+        return response
+    except Exception as e:
+        print(e)
+        
+        
+@app.post("/read-dividend-data")
+async def read_earning_data(request_body: RequestDataDividend):
+    try:
+        body_data = request_body
+        response = await get_dividend_data(body_data.tickers_name,body_data.start_date, body_data.end_date, body_data.query_type)
+        return response
+    except Exception as e:
+        print(e)
+
+
 
 
--- schema/request_data.py
+++ schema/request_data.py
@@ -28,6 +28,7 @@ class RequestDataBDH(BaseModel):
         ...,
         # example="['Security_Name', 'GICS_Sector_Name']",
     )
+    adjust: Optional[str] = None
     start_date: str = Field(
         default=default_start_date.strftime('%Y-%m-%d'),
         description="Date in 'yyyy-mm-dd' format (e.g., '2023-11-08')",
@@ -38,7 +39,104 @@ class RequestDataBDH(BaseModel):
         description="Date in 'yyyy-mm-dd' format (e.g., '2023-11-08')",
         example="yyy-mm-dd",
     )
+    
+    
+class RequestDataBDS(BaseModel):
+     # Calculate default dates one week prior
+    default_start_date: ClassVar[datetime] = datetime.now() - timedelta(weeks=1)
+    default_end_date: ClassVar[datetime] = datetime.now()
+    
+    tickers_name: List[str] = Field(
+        ...,
+        # example="['AAPL US Equity', 'NVDA US Equity']",
+    )
+    fields_name: List[str] = Field(
+        ...,
+        # example="['Security_Name', 'GICS_Sector_Name']",
+    )
+    start_date: str = Field(
+        default=default_start_date.strftime('%Y-%m-%d'),
+        description="Date in 'yyyy-mm-dd' format (e.g., '2023-11-08')",
+        example="yyy-mm-dd",
+    )
+    end_date: str = Field(
+        default=default_end_date.strftime('%Y-%m-%d'),
+        description="Date in 'yyyy-mm-dd' format (e.g., '2023-11-08')",
+        example="yyy-mm-dd",
+    )
+    
 
+
+class RequestDataBDIB(BaseModel):
+     # Calculate default dates one week prior
+    default_start_date: ClassVar[datetime] = datetime.now() - timedelta(weeks=2)
+    
+    ticker_name: str = Field(
+        ...,
+        example="BHP AU Equity",
+    )
+    session: Optional[str] = None
+    query_type: Optional[str] = None
+    date: str = Field(
+        default=default_start_date.strftime('%Y-%m-%d'),
+        description="Date in 'yyyy-mm-dd' format (e.g., '2023-11-08')",
+        example="yyy-mm-dd",
+    )
     
     
+
+class RequestDataEarning(BaseModel):
+     # Calculate default dates one week prior
+    default_start_date: ClassVar[datetime] = datetime.now() - timedelta(weeks=2)
+    
+    ticker_name: str = Field(
+        ...,
+        example="BHP AU Equity",
+    )
+    by: str  = Field(
+        ...,
+        example="Geo",
+    )
+    query_type: str = Field(
+        ...,
+        example="Revenue",
+    )
+    currency: Optional[str] = None
+    level: Optional[str] = None
+    date: Optional[int] = Field(
+        default=default_start_date.strftime('%Y'),
+        description="Date in 'yyyy' format (e.g., '2020')",
+        example="2018",
+    )
+    period: Optional[int] = Field(
+        example=1,
+    )
+   
+    
+    
+
+
+class RequestDataDividend(BaseModel):
+     # Calculate default dates one week prior
+    default_start_date: ClassVar[datetime] = datetime.now() - timedelta(weeks=1)
+    default_end_date: ClassVar[datetime] = datetime.now()
+    
+    tickers_name: List[str] = Field(
+        ...,
+        # example="['AAPL US Equity', 'NVDA US Equity']",
+    )
+    query_type: str = Field(
+        ...,
+        example="all",
+    )
+    start_date: str = Field(
+        default=default_start_date.strftime('%Y-%m-%d'),
+        description="Date in 'yyyy-mm-dd' format (e.g., '2023-11-08')",
+        example="yyy-mm-dd",
+    )
+    end_date: str = Field(
+        default=default_end_date.strftime('%Y-%m-%d'),
+        description="Date in 'yyyy-mm-dd' format (e.g., '2023-11-08')",
+        example="yyy-mm-dd",
+    )
     
