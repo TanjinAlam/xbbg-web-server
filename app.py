0b388d669f15fa71124f7f511f121e14d5bf2399
TanjinAlam <piash.tanjin@gmail.com>
2023-11-14 19:28:40 +0600

    radis added

--- app.py
+++ app.py
@@ -1,17 +1,32 @@
 from fastapi import FastAPI
 import pandas as pd
 from schema.request_data import RequestDataBDP, RequestDataBDH, RequestDataBDIB, RequestDataBDS, RequestDataDividend, RequestDataEarning
-from utility.helper import bdh_async, bdib_async, bdp_async, bds_async, earning_async, get_dividend_data
+from utility.blp_helper import bdh_async, bdib_async, bdp_async, bds_async, earning_async, get_dividend_data
 from fastapi.responses import JSONResponse
+import aioredis
 
+from utility.helper import get_array_from_redis_key, hash_object, set_array_as_redis_key
 app = FastAPI()
+redis = aioredis.from_url("redis://localhost", decode_responses=True)
+
 
 @app.post("/read-bdp-data")
 async def read_bdp_data(request_body: RequestDataBDP):
     try:
         body_data = request_body
-        response = await bdp_async(body_data.tickers_name,body_data.fields_name)
-        return JSONResponse(content=response, media_type="application/json")
+        
+        cache_hash_key = hash_object(body_data)
+        bdh_cache_data = await get_array_from_redis_key(redis, "bdp")
+        
+        if not bdh_cache_data or cache_hash_key not in bdh_cache_data:
+            bdh_cache_data.append(cache_hash_key)
+            response = await bdh_async(body_data.tickers_name,body_data.fields_name,body_data.start_date, body_data.end_date, request_body.adjust)
+            await set_array_as_redis_key(redis, "bdp", bdh_cache_data)
+            await redis.set(cache_hash_key, response)
+            return JSONResponse(content=response, media_type="application/json")
+                    
+        if cache_hash_key in bdh_cache_data:
+            return JSONResponse(content=await redis.get(cache_hash_key), media_type="application/json")
     except Exception as e:
         print(e)
 
@@ -19,36 +34,86 @@ async def read_bdp_data(request_body: RequestDataBDP):
 async def read_bdh_data(request_body: RequestDataBDH):
     try:
         body_data = request_body
-        response = await bdh_async(body_data.tickers_name,body_data.fields_name,body_data.start_date, body_data.end_date, request_body.adjust)
-        return JSONResponse(content=response, media_type="application/json")
+        
+        cache_hash_key = hash_object(body_data)
+        bdh_cache_data = await get_array_from_redis_key(redis, "bdh")
+        
+        if not bdh_cache_data or cache_hash_key not in bdh_cache_data:
+            bdh_cache_data.append(cache_hash_key)
+            response = await bdh_async(body_data.tickers_name,body_data.fields_name,body_data.start_date, body_data.end_date, request_body.adjust)
+            await set_array_as_redis_key(redis, "bdh", bdh_cache_data)
+            await redis.set(cache_hash_key, response)
+            return JSONResponse(content=response, media_type="application/json")
+                    
+        if cache_hash_key in bdh_cache_data:
+            return JSONResponse(content=await redis.get(cache_hash_key), media_type="application/json")
+
     except Exception as e:
         print(e)
-        
+
         
 @app.post("/read-bds-data")
 async def read_bds_data(request_body: RequestDataBDS):
     try:
         body_data = request_body
-        response = await bds_async(body_data.tickers_name,body_data.fields_name,body_data.start_date, body_data.end_date)
-        return JSONResponse(content=response, media_type="application/json")
+        
+        cache_hash_key = hash_object(body_data)
+        bdh_cache_data = await get_array_from_redis_key(redis, "bds")
+        
+        if not bdh_cache_data or cache_hash_key not in bdh_cache_data:
+            bdh_cache_data.append(cache_hash_key)
+            response = await bdh_async(body_data.tickers_name,body_data.fields_name,body_data.start_date, body_data.end_date, request_body.adjust)
+            await set_array_as_redis_key(redis, "bds", bdh_cache_data)
+            await redis.set(cache_hash_key, response)
+            return JSONResponse(content=response, media_type="application/json")
+                    
+        if cache_hash_key in bdh_cache_data:
+            return JSONResponse(content=await redis.get(cache_hash_key), media_type="application/json")
+
     except Exception as e:
         print(e)
         
 @app.post("/read-bdib-data")
 async def read_bdib_data(request_body: RequestDataBDIB):
     try:
+
         body_data = request_body
-        response = await bdib_async(body_data.ticker_name,body_data.session,body_data.query_type,body_data.date)
-        return JSONResponse(content=response, media_type="application/json")
+        
+        cache_hash_key = hash_object(body_data)
+        bdh_cache_data = await get_array_from_redis_key(redis, "bdib")
+        
+        if not bdh_cache_data or cache_hash_key not in bdh_cache_data:
+            bdh_cache_data.append(cache_hash_key)
+            response = await bdh_async(body_data.tickers_name,body_data.fields_name,body_data.start_date, body_data.end_date, request_body.adjust)
+            await set_array_as_redis_key(redis, "bdib", bdh_cache_data)
+            await redis.set(cache_hash_key, response)
+            return JSONResponse(content=response, media_type="application/json")
+                    
+        if cache_hash_key in bdh_cache_data:
+            return JSONResponse(content=await redis.get(cache_hash_key), media_type="application/json")
+        
     except Exception as e:
         print(e)
         
 @app.post("/read-earning-data")
 async def read_earning_data(request_body: RequestDataEarning):
     try:
+    
         body_data = request_body
-        response = await earning_async(body_data.ticker_name,body_data.by, body_data.query_type, body_data.currency, body_data.level, body_data.date, body_data.period)
-        return JSONResponse(content=response, media_type="application/json")
+        
+        cache_hash_key = hash_object(body_data)
+        bdh_cache_data = await get_array_from_redis_key(redis, "earning")
+        
+        if not bdh_cache_data or cache_hash_key not in bdh_cache_data:
+            bdh_cache_data.append(cache_hash_key)
+            response = await bdh_async(body_data.tickers_name,body_data.fields_name,body_data.start_date, body_data.end_date, request_body.adjust)
+            await set_array_as_redis_key(redis, "earning", bdh_cache_data)
+            await redis.set(cache_hash_key, response)
+            return JSONResponse(content=response, media_type="application/json")
+                    
+        if cache_hash_key in bdh_cache_data:
+            return JSONResponse(content=await redis.get(cache_hash_key), media_type="application/json")
+        
     except Exception as e:
         print(e)
         
@@ -56,9 +121,21 @@ async def read_earning_data(request_body: RequestDataEarning):
 @app.post("/read-dividend-data")
 async def read_earning_data(request_body: RequestDataDividend):
     try:
+        
         body_data = request_body
-        response = await get_dividend_data(body_data.tickers_name,body_data.start_date, body_data.end_date, body_data.query_type)
-        return JSONResponse(content=response, media_type="application/json")
+        
+        cache_hash_key = hash_object(body_data)
+        bdh_cache_data = await get_array_from_redis_key(redis, "dividend")
+        
+        if not bdh_cache_data or cache_hash_key not in bdh_cache_data:
+            bdh_cache_data.append(cache_hash_key)
+            response = await bdh_async(body_data.tickers_name,body_data.fields_name,body_data.start_date, body_data.end_date, request_body.adjust)
+            await set_array_as_redis_key(redis, "dividend", bdh_cache_data)
+            await redis.set(cache_hash_key, response)
+            return JSONResponse(content=response, media_type="application/json")
+                    
+        if cache_hash_key in bdh_cache_data:
+            return JSONResponse(content=await redis.get(cache_hash_key), media_type="application/json")
     except Exception as e:
         print(e)
 
--- utility/helper.py
+++ utility/helper.py
@@ -1,105 +1,32 @@
-import asyncio
-import pandas as pd
-from xbbg import blp
 
-async def bdp_async(tickers_name,flds):
-    return await asyncio.to_thread(get_bdp_data, tickers_name,flds)
-    
-async def bdh_async(tickers_name,flds, start_date, end_date, adjust):
-    return await asyncio.to_thread(get_bdh_data, tickers_name,flds, start_date, end_date, adjust)
-    
-async def bds_async(tickers_name,flds, start_date, end_date):
-    return await asyncio.to_thread(get_bds_data, tickers_name,flds, start_date, end_date)
-    
-async def bdib_async(ticker_name,session, query, date):
-    return await asyncio.to_thread(get_bdib_data, ticker_name,session, query, date)
-    
-async def earning_async(ticker_name,by, query, currency,level, date, period):
-    return await asyncio.to_thread(get_earning_data, ticker_name, by, query, currency,level, date, period)
+import json
+import pickle
+import hashlib
 
-async def dividend_async(tickers_name, start_date, end_date, query):
-    return await asyncio.to_thread(get_dividend_data, tickers_name, start_date, end_date,query)
-    
+def hash_object(obj):
+    # Serialize the object to a byte stream using pickle
+    obj_bytes = pickle.dumps(obj)
+
+    # Calculate the hash of the byte stream
+    obj_hash = hashlib.sha256(obj_bytes).hexdigest()
+
+    return obj_hash
 
 
-#to access xbbg specific function based custom paramters
-def get_bdp_data(tickers,flds):
-    requested_data = blp.bdp(tickers=tickers, flds=flds)
-    return requested_data
 
-def get_bdh_data(tickers,flds,start_date,end_date, adjust):
-    if adjust == "-" or None:
-        requested_data = blp.bdh(tickers=tickers, flds=flds, start_date=start_date, end_date=end_date)
+async def set_array_as_redis_key(redis, key, array):
+    # Serialize the array to JSON before storing it in Redis
+    serialized_array = json.dumps(array)
+    await redis.set(key, serialized_array)
+    await redis.expire(key, 7200)
+
+async def get_array_from_redis_key(redis, key):
+    # Retrieve the serialized array from Redis
+    serialized_array = await redis.get(key)
+
+    # Deserialize the array from JSON
+    if serialized_array:
+        array = json.loads(serialized_array)
+        return array
     else:
-         requested_data = blp.bdh(tickers=tickers, flds=flds, start_date=start_date, end_date=end_date,adjust=adjust)
-         
-    return requested_data.to_json()
-
-
-def get_bds_data(tickers,flds,start_date,end_date):
-    requested_data = blp.bds(tickers=tickers, flds=flds, DVD_Start_Dt=start_date, DVD_End_Dt=end_date)
-    
-    return requested_data.to_json()
-
-
-
-def get_bdib_data(ticker_name,session,query,date):
-    if session == None and query == None: 
-        requested_data = blp.bdib(ticker=ticker_name, dt=date)
-    elif session and query: 
-        requested_data = blp.bdib(ticker=ticker_name, dt=date, session=session, typ=query)
-    elif session: 
-        requested_data = blp.bdib(ticker=ticker_name, dt=date, session=session)
-    elif query: 
-        requested_data = blp.bdib(ticker=ticker_name, dt=date, session=session)
-    
-    return requested_data.to_json()
-
-
-
-def get_earning_data(ticker_name,by,query,currency,level, date , period):
-    if currency == None and level == None and date == None and period == None: 
-        requested_data = blp.bdib(ticker=ticker_name, by=by, typ=query)
-    elif currency and level and date and period: 
-        requested_data = blp.bdib(ticker=ticker_name, by=by, typ=query, Eqy_Fund_Year=date, level= level, ccy= currency, Number_Of_Periods= period)
-    elif currency and level and date and period:
-        requested_data = blp.bdib(ticker=ticker_name, by=by, typ=query, Eqy_Fund_Year=date, level=level, ccy=currency, Number_Of_Periods=period)
-    elif currency and level and date:
-        requested_data = blp.bdib(ticker=ticker_name, by=by, typ=query, Eqy_Fund_Year=date, level=level, ccy=currency)
-    elif currency and level and period:
-        requested_data = blp.bdib(ticker=ticker_name, by=by, typ=query, level=level, ccy=currency, Number_Of_Periods=period)
-    elif currency and date and period:
-        requested_data = blp.bdib(ticker=ticker_name, by=by, typ=query, Eqy_Fund_Year=date, ccy=currency, Number_Of_Periods=period)
-    elif level and date and period:
-        requested_data = blp.bdib(ticker=ticker_name, by=by, typ=query, Eqy_Fund_Year=date, level=level, Number_Of_Periods=period)
-    elif currency and level:
-        requested_data = blp.bdib(ticker=ticker_name, by=by, typ=query, level=level, ccy=currency)
-    elif currency and date:
-        requested_data = blp.bdib(ticker=ticker_name, by=by, typ=query, Eqy_Fund_Year=date, ccy=currency)
-    elif currency and period:
-        requested_data = blp.bdib(ticker=ticker_name, by=by, typ=query, ccy=currency, Number_Of_Periods=period)
-    elif level and date:
-        requested_data = blp.bdib(ticker=ticker_name, by=by, typ=query, Eqy_Fund_Year=date, level=level)
-    elif level and period:
-        requested_data = blp.bdib(ticker=ticker_name, by=by, typ=query, level=level, Number_Of_Periods=period)
-    elif date and period:
-        requested_data = blp.bdib(ticker=ticker_name, by=by, typ=query, Eqy_Fund_Year=date, Number_Of_Periods=period)
-    elif currency:
-        requested_data = blp.bdib(ticker=ticker_name, by=by, typ=query, ccy=currency)
-    elif level:
-        requested_data = blp.bdib(ticker=ticker_name, by=by, typ=query, level=level)
-    elif date:
-        requested_data = blp.bdib(ticker=ticker_name, by=by, typ=query, Eqy_Fund_Year=date)
-    elif period:
-        requested_data = blp.bdib(ticker=ticker_name, by=by, typ=query, Number_Of_Periods=period)
-    
-    return requested_data.to_json()
-
-
-def get_dividend_data(tickers,flds,start_date,end_date, query):
-    if query == "all" or None:
-        requested_data = blp.dividend(tickers=tickers, start_date=start_date, end_date=end_date)
-    else:
-         requested_data = blp.dividend(tickers=tickers, start_date=start_date, end_date=end_date,typ=query)
-    
-    return requested_data.to_json()
+        return []
